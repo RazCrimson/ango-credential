de81ba68ccb3d2241ef395876470ccc92ea25d07
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2023-02-07 00:42:00 +0530

    chg: FastAPI - add version in route paths

--- ango_credential/__main__.py
+++ ango_credential/__main__.py
@@ -6,8 +6,14 @@ from ango_credential.app.routers.credential import credential_router
 
 settings = Settings()
 
-app = FastAPI()
+app = FastAPI(
+    title="Ango - Credential microservice",
+    docs_url=f"{settings.APP_PREFIX.rstrip('/')}/docs",
+    redoc_url=f"{settings.APP_PREFIX.rstrip('/')}/redoc",
+    openapi_url=f"{settings.APP_PREFIX.rstrip('/')}/openapi.json",
+)
 
-app.include_router(credential_router, prefix="/credential")
+app.include_router(credential_router, prefix=f"{settings.APP_PREFIX.rstrip('/')}/v1", tags=["Credential API V1"])
 
-uvicorn.run(app, host=settings.SERVER_HOST, port=settings.SERVER_PORT)
+if __name__ == "__main__":
+    uvicorn.run(app, host=settings.SERVER_HOST, port=settings.SERVER_PORT)
