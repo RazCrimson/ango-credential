fd0b63ac4768cabfc83f874651e323870a876ebd
Saketh Raman KS <19pw26@psgtech.ac.in>
2023-02-06 18:37:17 +0530

    fix: router in __main__.py

--- ango_credential/__main__.py
+++ ango_credential/__main__.py
@@ -2,12 +2,12 @@ import uvicorn
 from fastapi import FastAPI
 
 from ango_credential.app.core.config import Settings
-from ango_credential.app.routers.user import user_router
+from ango_credential.app.routers.credential import credential_router
 
 settings = Settings()
 
 app = FastAPI()
 
-app.include_router(user_router, prefix="/credential")
+app.include_router(credential_router, prefix="/credential")
 
 uvicorn.run(app, host=settings.SERVER_HOST, port=settings.SERVER_PORT)
